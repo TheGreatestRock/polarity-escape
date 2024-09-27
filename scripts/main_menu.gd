539e4480a9a20dac10e4062a226c5cdfe3dae867
TheGreatestRock <vic38100@gmail.com>
2024-09-27 09:50:16 -0400

    init

---
+++ scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	pass #get_tree().change_scene_to_file(scene)
+
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
