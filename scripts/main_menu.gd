24b5a3832a71ef3b0094e9f227feefbf947f70c7
TheGreatestRock <vic38100@gmail.com>
2024-09-27 11:02:25 -0400

    killzone work

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,9 +4,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	pass #get_tree().change_scene_to_file(scene)
-
-
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
