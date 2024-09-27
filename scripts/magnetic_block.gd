3dc94daa441b5d6367414fc235820d299d88fb09
TheGreatestRock <vic38100@gmail.com>
2024-09-27 15:42:08 -0400

    update

--- scripts/magnetic_block.gd
+++ scripts/magnetic_block.gd
@@ -4,7 +4,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body, collision_layer, collision_mask)
 	collision_layer = 3
 
-
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	print(body, collision_layer)
+	print(body, collision_layer, collision_mask)
 	collision_layer = 2
