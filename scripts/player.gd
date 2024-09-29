7419955b151fa3d050620c9da44a308e25acc63e
TheGreatestRock <vic38100@gmail.com>
2024-09-29 14:27:42 -0400

    e

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ var HAS_MAGNET = false
 var POLARITY = 0 # 1 for positive, -1 for negative
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @onready var hud: CanvasLayer = $"../HUD"
 
@@ -38,10 +39,10 @@ func _physics_process(delta: float) -> void:
 		# Add the gravity.
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+			
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
