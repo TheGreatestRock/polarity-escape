a5d4d6e71813805f150a03bdfdd45319b37c7e33
TheGreatestRock <vic38100@gmail.com>
2024-09-29 11:38:49 -0400

    fixed polarity area bug

--- scripts/polarity_area.gd
+++ scripts/polarity_area.gd
@@ -4,20 +4,24 @@ extends Area2D
 
 @export var POLARITY: int = -1
 
-func _physics_process(delta: float) -> void:
-	pass
+var player_in_area: bool = false
 
+func _physics_process(delta: float) -> void:
+	if player_in_area == true:
+		update_jump_velocity(player)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D and body.HAS_MAGNET == true and body == player:
-		#check if the player has the same polarity as the area
-		if body.POLARITY == POLARITY and body.HAS_MAGNET == true:
-			#decrease the polarity of the player
-			body.JUMP_VELOCITY = body.JUMP_VELOCITY * 2
-		elif body.POLARITY != POLARITY and body.HAS_MAGNET == true:
-			#increase the polarity of the player
-			body.JUMP_VELOCITY = body.JUMP_VELOCITY / 2
+		player_in_area = true
+		update_jump_velocity(body)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
+		player_in_area = false
 		body.JUMP_VELOCITY = -300.0
+
+func update_jump_velocity(body: CharacterBody2D) -> void:
+	if body.POLARITY == POLARITY and body.HAS_MAGNET == true:
+		body.JUMP_VELOCITY = -600
+	elif body.POLARITY != POLARITY and body.HAS_MAGNET == true:
+		body.JUMP_VELOCITY = -150
